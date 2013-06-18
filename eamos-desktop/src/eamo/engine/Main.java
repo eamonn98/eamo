e3d59d76e836409d240efffe77100334ccbcdb88
eamonn98 <eamonn98@gmail.com>
2013-06-18 19:23:32 +0100

    Initial

---
+++ eamos-desktop/src/eamo/engine/Main.java
@@ -0,0 +1,16 @@
+package eamo.engine;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "eamos";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new Examples(), cfg);
+	}
+}
